1116ee9fd27e43f5342f1a7beebb9f4cfe759c8d
Naziarichi <naziarichi887@gmail.com>
2025-10-22 16:05:51 +0600

    Add files via upload

---
+++ q2.dart
@@ -0,0 +1,18 @@
+
+void printEvenNumbers(int start, int end) {
+  print("Even numbers between $start and $end:");
+  for (int i = start; i <= end; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
+}
+
+void main() {
+  
+  int start = 10;
+  int end = 30;
+
+  
+  printEvenNumbers(start, end);
+}
