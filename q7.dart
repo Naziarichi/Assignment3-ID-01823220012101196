9e60c495e07c0be3e2cdfeacbdd5d23dde85498a
Naziarichi <naziarichi887@gmail.com>
2025-10-22 16:15:50 +0600

    Add files via upload

---
+++ q7.dart
@@ -0,0 +1,14 @@
+
+num add(num a, num b) {
+  return a + b;
+}
+
+void main() {
+ 
+  num number1 = 10;
+  num number2 = 20;
+
+  num result = add(number1, number2);
+
+  print("The sum of $number1 and $number2 is $result");
+}
